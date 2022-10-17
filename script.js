0bf21681b537e5931110c5ee85b14fcd17d6b808
Mukaramatu Umar Farouk <110169851+mukarama-farouk@users.noreply.github.com>
2022-10-17 17:05:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let js = "amazing";
+if (js === "amazing") alert("javascript is fun!");
+console.log(30 - 90 + 60 * 3);
+
+let q = 15 + 5 - 8;
+console.log(q);
+
+let p = 12 - 9 + 1 * 90;
+console.log(p);
+
+console.log("RAMA");
+
+console.log(58);
+
+let firstname = "RAMA";
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+var m = 30;
+m += 50;
+console.log(m);
+
+var r = 40;
+r -= 10;
+console.log(r);
+
+var a = 30;
+a %= 20;
+console.log(a);
+
+var t = 30;
+if (t < 10) {
+  document.write("value of t is greater than 10");
+}
+
+var t = 30;
+if (t > 10) {
+  document.write("value of t is greater than 10");
+}
+
+var j = 15;
+if (j % 3 == 0) {
+  document.write("j is an even number");
+} else document.write("j is an odd number");
